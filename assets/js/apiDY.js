355d2fd64138e12e6346bc9065918e984c288a45
Saul Ackerman <newaccount@Sauls-MacBook-Air.local>
2019-02-07 09:54:06 -0500

    f

--- assets/js/apiDY.js
+++ assets/js/apiDY.js
@@ -1,33 +1,33 @@
-let obj;
-const DYAPICall = (event) => {
-    event.preventDefault();
-     obj = {
-        name: "Add To Cart",
-        properties: {
-            dyType: "add-to-cart-v1", // Do Not Change
-            productId: "sku-4324-bg",
-            value: 12.34, // Total cart value
-            currency: "any supported currency code", // For multiple currencies
-            quantity: 2,
-          size: "M",
-          cart: [
-             {
-               productId: "item-34454", // SKU exactly as in the product feed
-               quantity: 1, // Must be a Int
-               itemPrice: 65.87, //Must be a float
-               size: "XL", // Size is optional
-             },
-             {
-               productId: "sku-4324-bg", // SKU exactly as in the product feed
-               quantity: 2, // Must be a Int
-               itemPrice: 12.34, //Must be a float
-               size: "M", // Size is optional
-             }
+// let obj;
+// const DYAPICall = (event) => {
+//     event.preventDefault();
+//      obj = {
+//         name: "Add To Cart",
+//         properties: {
+//             dyType: "add-to-cart-v1", // Do Not Change
+//             productId: "sku-4324-bg",
+//             value: 12.34, // Total cart value
+//             currency: "any supported currency code", // For multiple currencies
+//             quantity: 2,
+//           size: "M",
+//           cart: [
+//              {
+//                productId: "item-34454", // SKU exactly as in the product feed
+//                quantity: 1, // Must be a Int
+//                itemPrice: 65.87, //Must be a float
+//                size: "XL", // Size is optional
+//              },
+//              {
+//                productId: "sku-4324-bg", // SKU exactly as in the product feed
+//                quantity: 2, // Must be a Int
+//                itemPrice: 12.34, //Must be a float
+//                size: "M", // Size is optional
+//              }
 
-        }
-    }
+//         }
+//     }
 
-console.log(obj);
+// console.log(obj);
 
 
-} 
+// } 
